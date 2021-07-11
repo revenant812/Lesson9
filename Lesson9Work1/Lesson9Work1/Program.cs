aaaa512827f632ac701b761635b39110eb3cf43c
[Eskin Dmitriy] <9255151@mail.ru>
2021-07-11 22:24:42 +0300

    Практическое задание номер 9

--- Lesson9Work1/Lesson9Work1/Program.cs
+++ Lesson9Work1/Lesson9Work1/Program.cs
@@ -8,64 +8,22 @@ namespace Lesson9Work1
     {
         static int count = 0;
         public static string StartString = @"C:\Users\Дима\Desktop\TestDir";
+        
         static void Main(string[] args)
         {
-            Tree(StartString);
+           
+            TreeLite(StartString);
             Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine();
-            while (true)
-            {                
+            while(true)
+            {
                 Console.WriteLine();
-                FontColorGreen("Введите команду: ");             
+                FontColorGreen("Введите команду: ");
                 string command = Console.ReadLine();
-                if (command.Contains("copy"))
-                {
-                    FontColorGreen("Что скопировать: ");
-                    string FromStr = Console.ReadLine();
-                    FontColorGreen("Куда скопировать: ");
-                    string ToStr = Console.ReadLine();
-                    try
-                    {
-                        CopyDir(FromStr, ToStr, StartString);
-                    }
-                    catch
-                    {
-                        Console.WriteLine("Ошибка в пути к папке!");
-                        Console.WriteLine();
-                        Console.WriteLine();
-                        Console.WriteLine();
-                    }
-                }
-                if (command.Contains("cd"))
-                {                   
-                        FontColorGreen("Куда перейти: ");
-                        string ChangeStr = Console.ReadLine();
-                        StartString = ChangeDirToUp(StartString, ChangeStr);
-                        Tree(StartString);
-                        Console.WriteLine();
-                        Console.WriteLine();
-                        Console.WriteLine();                  
-                }
-                if (command.Contains(".."))
-                {                                   
-                        Console.WriteLine();
-                        StartString = ChangeDirToDown(StartString);
-                        Tree(StartString);
-                        Console.WriteLine();
-                        Console.WriteLine();
-                        Console.WriteLine();                    
-                }
-                if (command.Contains("ls"))
-                {                    
-                    Console.WriteLine();
-                    Tree(StartString);
-                    Console.WriteLine();
-                    Console.WriteLine();
-                    Console.WriteLine();
-                }
-
-            }            
+                ParserCom(command);
+            }
+         
         }
         static void FontColorRed(string str)
         {
@@ -81,31 +39,25 @@ namespace Lesson9Work1
             Console.ResetColor();
         }
 
-        public static string TreeLite(string str)
+        static void FontColorYellow(string str)
         {
-            DirectoryInfo Dir = new DirectoryInfo(str);
-            string result = Convert.ToString(Dir);
-            //var ArrayDir = Dir.GetDirectories();
-            return result;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(str);
+            Console.ResetColor();
         }
+
+
         public static void Tree(string str)
         {
             int MacCountElem = (Console.LargestWindowHeight - 10);
             try
             {
                 DirectoryInfo Dir1 = new DirectoryInfo(str);
-                var ArrayDir1 = Dir1.GetDirectories();
+                var ArrayDir1 = Dir1.GetDirectories();            
                 Console.WriteLine(Dir1);
                 foreach (DirectoryInfo DirLayer1 in ArrayDir1)
                 {
-                    count = count + 1;
-                    /*
-                    if(count > MacCountElem)
-                    {
-                        Console.WriteLine("Элементов больше 40");
-                        return;
-                    }
-                    */
+                    count = count + 1;               
                     Console.WriteLine($"|-----{DirLayer1.Name}");
                     try
                     {
@@ -113,14 +65,7 @@ namespace Lesson9Work1
                         var ArrayDir2 = Dir2.GetDirectories();
                         foreach (DirectoryInfo DirLayer2 in ArrayDir2)
                         {
-                            count = count + 1;
-                            /*
-                            if (count > MacCountElem)
-                            {
-                                Console.WriteLine("Элементов больше 40");
-                                return;
-                            }
-                            */
+                            count = count + 1;                           
                             Console.WriteLine($"     |-----{DirLayer2.Name}");
                             try
                             {
@@ -128,14 +73,7 @@ namespace Lesson9Work1
                                 var ArrayDir3 = Dir3.GetDirectories();
                                 foreach (DirectoryInfo DirLayer3 in ArrayDir3)
                                 {
-                                    count = count + 1;
-                                    /*
-                                    if (count > MacCountElem)
-                                    {
-                                        Console.WriteLine("Элементов больше 40");
-                                        return;
-                                    }
-                                    */
+                                    count = count + 1;                                   
                                     Console.WriteLine($"          |-----{DirLayer3.Name}");
                                 }
                             }
@@ -150,6 +88,15 @@ namespace Lesson9Work1
                         FontColorRed("     |-----Нет доступ к папке");                   
                     }
                 }
+                
+                foreach (FileInfo file1 in Dir1.GetFiles())
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"|{file1.Name}");
+                    Console.ResetColor();
+                }
+                
+
             }
 
             catch
@@ -159,48 +106,43 @@ namespace Lesson9Work1
             count = 0;
         }
 
-        static void LineMenu(string str)
+        public static void TreeLite(string str)
         {
-            Console.SetCursorPosition(0, 0);
-            var originalpos = Console.CursorTop;
-            var k = Console.ReadKey();
-            var i = 0;
-            while (k.KeyChar != 'q')
+            DirectoryInfo Dir = new DirectoryInfo(str);
+            var ArrayDir = Dir.GetDirectories();
+            Console.WriteLine(Dir);
+            foreach (DirectoryInfo DirLayer1 in ArrayDir)
             {
-                if (k.Key == ConsoleKey.DownArrow)
-                {
-                    Console.SetCursorPosition(0, Console.CursorTop + i);                         
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    Console.BackgroundColor = ConsoleColor.White;
-                    Console.WriteLine(str);
-                    Console.ResetColor();
-                    i++;
 
-                }
-
-                //Console.SetCursorPosition(0, originalpos);
-                k = Console.ReadKey();
+                Console.WriteLine($"|-----{DirLayer1.Name}");
             }
-        }
-
-        static void CopyDir(string FromDir, string ToDir, string StartDirFoo)
-        {
-            Directory.CreateDirectory(ToDir + "\\" + FromDir);
-            string newstring = (StartDirFoo + "\\" + FromDir);            
-            foreach (string s1 in Directory.GetFiles(newstring))
+            //Directory.GetFiles(str);
+            
+            foreach(FileInfo file in Dir.GetFiles())
             {
-                string s2 = ToDir + "\\" + FromDir + "\\" + Path.GetFileName(s1);
+                Console.ForegroundColor = ConsoleColor.Yellow;                
+                Console.WriteLine($"|{file.Name}");             
+                Console.ResetColor();
+            }
+            
+        }
+              
+        static void CopyDir(string FromDir, string ToDir)
+        {
+            Directory.CreateDirectory(ToDir);                        
+            foreach (string s1 in Directory.GetFiles(FromDir))
+            {               
+                string s2 = ToDir + "\\" + Path.GetFileName(s1);
                 File.Copy(s1, s2);
             }
-            foreach (string s in Directory.GetDirectories(newstring))
+            foreach (string s in Directory.GetDirectories(FromDir))
             {
-                CopyDir(s, ToDir + "\\" + FromDir + "\\" + Path.GetFileName(s), StartDirFoo);
+                CopyDir(s, ToDir + "\\" + Path.GetFileName(s));                
             }
         }
 
         public static string ChangeDirToUp(string ChangeFromDir, string ChangetoDir)
         {
-            //DirectoryInfo p = new DirectoryInfo(ChangeFromDir);
             string newstring = (ChangeFromDir + "\\" + ChangetoDir);
             if(Directory.Exists(newstring))
             {
@@ -222,6 +164,75 @@ namespace Lesson9Work1
             return newstring;
         }
 
+        public static void StatInfo(string str, string str1)
+        {
+            DirectoryInfo Dir = new DirectoryInfo(str);
+            foreach (FileInfo file in Dir.GetFiles())
+            {
+                if(file.Name.Equals(str1))
+                {
+                    Console.WriteLine(file.CreationTime);
+                    Console.WriteLine(file.Length + " " + "byte");                  
+                }
+            }
+        }
 
+        public static void ParserCom(string FullCom)
+        {
+            
+            string[] array = FullCom.Split(' ');
+            if(array[0].Equals("ls"))
+            {
+                Console.WriteLine();
+                Tree(StartString);
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            else if(array[0].Equals("cd"))
+            {
+
+                string ChangeStr = array[1];
+                StartString = ChangeDirToUp(StartString, ChangeStr);
+                Tree(StartString);
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            else if(array[0].Equals(".."))
+            {
+                Console.WriteLine();
+                StartString = ChangeDirToDown(StartString);
+                Tree(StartString);
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            else if(array[0].Equals("copy"))
+            {
+                array[1] = StartString + "\\" + array[1];                
+                try
+                {
+                    CopyDir(array[1], array[2]);                  
+                }
+                catch
+                {
+                    Console.WriteLine("Ошибка в пути к папке!");
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine();
+                }
+                Tree(StartString);
+            }
+            else if (array[0].Equals("stat"))
+            {
+                StatInfo(StartString, array[1]);
+                //Tree(StartString);
+            }
+            else
+            {
+                Console.WriteLine("Не правильная команда.");
+            }
+        }
     }
 }
